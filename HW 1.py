5afe8ab2096efad491e60e7c43b55136417a85ac
Miki-Uk <123359607+Miki-Uk@users.noreply.github.com>
2024-03-23 09:58:34 +0200

    my homework

---
+++ HW 1.py
@@ -0,0 +1,4 @@
+name = input("Write your name ")
+sub = input("what is your favourite subject in school")
+mark = input( "what is your mark ")
+print("Your name ", name, "Your subject", sub, "Your marks", mark)
